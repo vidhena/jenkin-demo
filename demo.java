91bf0a921302fe581b80a0daa338b1ccb8e4b283
vidhena <vidhena16@gmail.com>
2026-06-08 18:28:22 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,1 +1,9 @@
-
+public class demo {
+    
+    // The main method is the execution entry point for your application
+    public static void main(String[] args) {
+        
+        // Prints the string text directly to your console
+        System.out.println("Hello");
+    }
+}
